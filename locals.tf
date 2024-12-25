4dd07e6aa8b7ea76199453cdcca152526f2f10d2
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-12-25 21:45:59 +0530

    Update ami_id

--- locals.tf
+++ locals.tf
@@ -2,6 +2,6 @@ locals {
   main_vpc_cidr_block      = "192.168.0.0/16"
   public_subnet_cidr_block = "192.168.0.0/24"
   availability_zone_1      = "us-east-1a"
-  ami_id                   = "ami-0e670eb768a5fc3d4"
+  ami_id                   = "ami-0c02fb55956c7d316"
   instance_type            = "t2.micro"
 }
