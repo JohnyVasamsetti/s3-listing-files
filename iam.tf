c5532294c2204b75adc74eacd8bbf5aaf7d67b59
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-12-26 00:59:02 +0530

    Create role and policy for instance profile

---
+++ iam.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_role" "ec2_s3_role" {
+  name = "ec2-s3-access-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "s3_policy" {
+  name        = "ec2-s3-access-policy"
+  description = "Allow EC2 to access S3"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject"
+        ]
+        Resource = [
+          "${aws_s3_bucket.content_storage_for_listing_assignment.arn}",
+          "${aws_s3_bucket.content_storage_for_listing_assignment.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
+  role       = aws_iam_role.ec2_s3_role.name
+  policy_arn = aws_iam_policy.s3_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ec2_s3_role.name
+}
+
